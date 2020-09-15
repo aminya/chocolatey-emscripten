b001fa8daf63faa58d5dc3b27ec5f8919be898b6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-15 00:48:33 -0500

    add try-catch for remove_envs in case choco is not installed

--- tools/remove_envs.ps1
+++ tools/remove_envs.ps1
@@ -1,12 +1,26 @@
 write-host "Removing environment variables" -ForegroundColor Blue
-Uninstall-ChocolateyEnvironmentVariable "EMSDK" -VariableType User
-Uninstall-ChocolateyEnvironmentVariable "EMSDK_NODE" -VariableType User
-Uninstall-ChocolateyEnvironmentVariable "JAVA_HOME" -VariableType User
-Uninstall-ChocolateyEnvironmentVariable "EMSDK_PYTHON" -VariableType User
-Uninstall-ChocolateyEnvironmentVariable "EM_CACHE" -VariableType User
-# if added by emsdk itself it will be Machine
-Uninstall-ChocolateyEnvironmentVariable "EMSDK" -VariableType Machine
-Uninstall-ChocolateyEnvironmentVariable "EMSDK_NODE" -VariableType Machine
-Uninstall-ChocolateyEnvironmentVariable "JAVA_HOME" -VariableType Machine
-Uninstall-ChocolateyEnvironmentVariable "EMSDK_PYTHON" -VariableType Machine
-Uninstall-ChocolateyEnvironmentVariable "EM_CACHE" -VariableType Machine
+try {
+  Uninstall-ChocolateyEnvironmentVariable "EMSDK" -VariableType User
+  Uninstall-ChocolateyEnvironmentVariable "EMSDK_NODE" -VariableType User
+  Uninstall-ChocolateyEnvironmentVariable "JAVA_HOME" -VariableType User
+  Uninstall-ChocolateyEnvironmentVariable "EMSDK_PYTHON" -VariableType User
+  Uninstall-ChocolateyEnvironmentVariable "EM_CACHE" -VariableType User
+  # if added by emsdk itself it will be Machine
+  Uninstall-ChocolateyEnvironmentVariable "EMSDK" -VariableType Machine
+  Uninstall-ChocolateyEnvironmentVariable "EMSDK_NODE" -VariableType Machine
+  Uninstall-ChocolateyEnvironmentVariable "JAVA_HOME" -VariableType Machine
+  Uninstall-ChocolateyEnvironmentVariable "EMSDK_PYTHON" -VariableType Machine
+  Uninstall-ChocolateyEnvironmentVariable "EM_CACHE" -VariableType Machine
+} catch {
+  [Environment]::SetEnvironmentVariable("EMSDK", $null, "User")
+  [Environment]::SetEnvironmentVariable("EMSDK_NODE", $null, "User")
+  [Environment]::SetEnvironmentVariable("JAVA_HOME", $null, "User")
+  [Environment]::SetEnvironmentVariable("EMSDK_PYTHON", $null, "User")
+  [Environment]::SetEnvironmentVariable("EM_CACHE", $null, "User")
+  # if added by emsdk itself it will be Machine
+  [Environment]::SetEnvironmentVariable("EMSDK", $null, "Machine")
+  [Environment]::SetEnvironmentVariable("EMSDK_NODE", $null, "Machine")
+  [Environment]::SetEnvironmentVariable("JAVA_HOME", $null, "Machine")
+  [Environment]::SetEnvironmentVariable("EMSDK_PYTHON", $null, "Machine")
+  [Environment]::SetEnvironmentVariable("EM_CACHE", $null, "Machine")
+}
