29c27d91b82dbb0dc35fa4a5b03fcc77b0289592
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-11-29 04:36:04 -0600

    add uninstaller for old versions

--- tools/remove_envs.ps1
+++ tools/remove_envs.ps1
@@ -6,6 +6,11 @@ try {
   Uninstall-ChocolateyEnvironmentVariable "EMSDK_PYTHON" -VariableType User
   Uninstall-ChocolateyEnvironmentVariable "EM_CACHE" -VariableType User
   Uninstall-ChocolateyEnvironmentVariable "EM_CONFIG" -VariableType User
+  Uninstall-ChocolateyEnvironmentVariable "BINARYEN_ROOT" -VariableType USER
+  Uninstall-ChocolateyEnvironmentVariable "EMSCRIPTEN" -VariableType USER
+  Uninstall-ChocolateyEnvironmentVariable "EMSCRIPTEN_NATIVE_OPTIMIZER" -VariableType USER
+  Uninstall-ChocolateyEnvironmentVariable "EMCC_WASM_BACKEND" -VariableType USER
+
   # if added by emsdk itself it will be Machine
   Uninstall-ChocolateyEnvironmentVariable "EMSDK" -VariableType Machine
   Uninstall-ChocolateyEnvironmentVariable "EMSDK_NODE" -VariableType Machine
@@ -13,6 +18,11 @@ try {
   Uninstall-ChocolateyEnvironmentVariable "EMSDK_PYTHON" -VariableType Machine
   Uninstall-ChocolateyEnvironmentVariable "EM_CACHE" -VariableType Machine
   Uninstall-ChocolateyEnvironmentVariable "EM_CONFIG" -VariableType Machine
+  Uninstall-ChocolateyEnvironmentVariable "BINARYEN_ROOT" -VariableType Machine
+  Uninstall-ChocolateyEnvironmentVariable "EMSCRIPTEN" -VariableType Machine
+  Uninstall-ChocolateyEnvironmentVariable "EMSCRIPTEN_NATIVE_OPTIMIZER" -VariableType Machine
+  Uninstall-ChocolateyEnvironmentVariable "EMCC_WASM_BACKEND" -VariableType Machine
+
 } catch {
   [Environment]::SetEnvironmentVariable("EMSDK", $null, "User")
   [Environment]::SetEnvironmentVariable("EMSDK_NODE", $null, "User")
@@ -20,6 +30,11 @@ try {
   [Environment]::SetEnvironmentVariable("EMSDK_PYTHON", $null, "User")
   [Environment]::SetEnvironmentVariable("EM_CACHE", $null, "User")
   [Environment]::SetEnvironmentVariable("EM_CONFIG", $null, "User")
+  [Environment]::SetEnvironmentVariable("BINARYEN_ROOT", $null, "User")
+  [Environment]::SetEnvironmentVariable("EMSCRIPTEN", $null, "User")
+  [Environment]::SetEnvironmentVariable("EMSCRIPTEN_NATIVE_OPTIMIZER", $null, "User")
+  [Environment]::SetEnvironmentVariable("EMCC_WASM_BACKEND", $null, "User")
+
   # if added by emsdk itself it will be Machine
   [Environment]::SetEnvironmentVariable("EMSDK", $null, "Machine")
   [Environment]::SetEnvironmentVariable("EMSDK_NODE", $null, "Machine")
@@ -27,4 +42,9 @@ try {
   [Environment]::SetEnvironmentVariable("EMSDK_PYTHON", $null, "Machine")
   [Environment]::SetEnvironmentVariable("EM_CACHE", $null, "Machine")
   [Environment]::SetEnvironmentVariable("EM_CONFIG", $null, "Machine")
+  [Environment]::SetEnvironmentVariable("BINARYEN_ROOT", $null, "Machine")
+  [Environment]::SetEnvironmentVariable("EMSCRIPTEN", $null, "Machine")
+  [Environment]::SetEnvironmentVariable("EMSCRIPTEN_NATIVE_OPTIMIZER", $null, "Machine")
+  [Environment]::SetEnvironmentVariable("EMCC_WASM_BACKEND", $null, "Machine")
+
 }
